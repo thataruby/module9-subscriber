7a2ef2689413453145610df0edb5b62b1f2013d2
thataruby <thata@dfi.id>
2025-05-15 10:45:13 +0700

    Initial subscriber code.

---
+++ src/main.rs
@@ -0,0 +1,44 @@
+use borsh::{BorshDeserialize, BorshSerialize};
+use crosstown_bus::{CrosstownBus, MessageHandler, HandleError};
+use std::time;
+
+#[derive(Debug, Clone, BorshDeserialize, BorshSerialize)]
+pub struct UserCreatedEventMessage {
+    pub user_id: String,
+    pub user_name: String,
+}
+
+pub struct UserCreatedHandler;
+
+impl MessageHandler<UserCreatedEventMessage> for UserCreatedHandler {
+    fn handle(&self, message: Box<UserCreatedEventMessage>) -> Result<(), HandleError> {
+        let _ten_millis = time::Duration::from_millis(1000);
+        let _now = time::Instant::now();
+
+        // thread::sleep(_ten_millis);
+
+        println!("In Thata's Computer [2306173113]. Message received: {:?}", message);
+        Ok(())
+    }
+
+        fn get_handler_action(&self) -> String {
+        "process_user_created".to_string()
+    }
+}
+
+fn main() {
+    let listener = CrosstownBus::new_queue_listener("amqp://guest:guest@localhost:5672".to_owned())
+        .unwrap();
+
+    _ = listener.listen(
+        "user_created".to_owned(),
+        UserCreatedHandler {},
+        crosstown_bus::QueueProperties {
+            auto_delete: false,
+            durable: false,
+            use_dead_letter: true,
+        },
+    );
+
+    loop {}
+}
